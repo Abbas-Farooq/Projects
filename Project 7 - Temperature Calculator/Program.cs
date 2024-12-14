de7e88922299f64b5b0a24636416fe72438b0c0b
Abbas <Farooqab103@gmail.com>
2024-12-14 20:09:38 +0000

    Added Project 7

---
+++ Project 7 - Temperature Calculator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to the Temperature Converter");
+        Console.WriteLine("1 - Celsius to Fahrenheit");
+        Console.WriteLine("2 - Fahrenheit to Celsius");
+        Console.Write("Pick 1 or 2: ");
+        int choice = Convert.ToInt32(Console.ReadLine());
+
+        if (choice == 1)
+        {
+            Console.WriteLine("Write your temperature");
+            double celsius = Convert.ToDouble(Console.ReadLine());
+            double fahrenheit = (celsius * 9 / 5) + 32;
+            Console.WriteLine($"{fahrenheit}°F is equal to {celsius}°C");
+        }
+
+        else if (choice == 2) 
+        {
+            Console.WriteLine("Write your temperature");
+            double celsius = Convert.ToDouble(Console.ReadLine());
+            double fahrenheit = (celsius * 9 / 5) + 32;
+            Console.WriteLine($"{celsius}°C is equal to {fahrenheit}°F");
+        }
+
+        else
+        {
+            Console.WriteLine("Invalid Choice");
+        }        
+            
+        
+    }
+}
---
+++ Project 7 - Temperature Calculator/README.md
@@ -0,0 +1,21 @@
+# Temperature Converter Program
+## What It Does:
+The Temperature Converter program lets you convert temperatures between Celsius and Fahrenheit. You can choose to either:
+
+Convert from Celsius to Fahrenheit, or
+Convert from Fahrenheit to Celsius.
+## How It Works:
+User Input: The program asks you to choose which type of conversion you want:
+
+Option 1: Celsius to Fahrenheit
+Option 2: Fahrenheit to Celsius After you choose, the program will ask you to enter a temperature to convert.
+
+## Key Concepts Used:
+User Input: The program takes your temperature input using Console.ReadLine().
+Math: It uses simple math to convert the temperatures.
+If Statements: It decides which formula to use based on your choice.
+
+## Extensions You Could Add:
+Multiple Conversions: Let the user do more than one conversion without restarting.
+Input Validation: Make sure the user enters a valid number.
+Error Messages: Show a message if the user selects an invalid option.
